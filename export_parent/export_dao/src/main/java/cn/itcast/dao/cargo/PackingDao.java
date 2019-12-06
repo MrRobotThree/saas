19d7d00c1b29ede9a71059526d9892a26efc2f1e
gjh <15735692606@163.com>
2019-12-06 11:04:08 +0800

    111111

---
+++ export_parent/export_dao/src/main/java/cn/itcast/dao/cargo/PackingDao.java
@@ -0,0 +1,22 @@
+package cn.itcast.dao.cargo;
+
+
+import cn.itcast.domain.cargo.Packing;
+
+import java.util.List;
+
+public interface PackingDao {
+    int deleteByPrimaryKey(String packingListId);
+
+    int insert(Packing record);
+
+    int insertSelective(Packing record);
+
+    Packing selectByPrimaryKey(String packingListId);
+
+    int updateByPrimaryKeySelective(Packing record);
+
+    int updateByPrimaryKey(Packing record);
+
+    List<Packing> findByCompanyId(String id);
+}
