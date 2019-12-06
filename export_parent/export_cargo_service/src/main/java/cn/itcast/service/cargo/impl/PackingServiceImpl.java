3c1d3c3039959f691dfddb35f01a970fe43c3056
gjh <15735692606@163.com>
2019-12-06 11:00:26 +0800

    1

---
+++ export_parent/export_cargo_service/src/main/java/cn/itcast/service/cargo/impl/PackingServiceImpl.java
@@ -0,0 +1,31 @@
+package cn.itcast.service.cargo.impl;
+
+import cn.itcast.dao.cargo.PackingDao;
+import cn.itcast.domain.cargo.Packing;
+import cn.itcast.service.cargo.PackingService;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+
+@Service
+public class PackingServiceImpl implements PackingService {
+
+    @Autowired
+    private PackingDao packingDao;
+    @Override
+    public PageInfo findAll(String id,int pageNum, int size) {
+        PageHelper.startPage(pageNum,size);
+        List<Packing> list = packingDao.findByCompanyId(id);
+
+        return new PageInfo(list);
+    }
+
+    @Override
+    public void save(Packing packing) {
+        packingDao.insertSelective(packing);
+    }
+}
